a970ef390373df794d8c4b43b3ca032fbb5e39ce
Ezra-10 <ekezia543@gmail.com>
2026-04-17 13:56:34 +0700

    tampilan awal

---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import Create from "./Create";
+import './App.css';
+
+
+function Home() {
+
+    return (
+        <div className="home">
+            <div className="container">
+                <h1>Team Task Manager</h1>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
